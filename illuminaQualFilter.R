7fc668501b2d8db39d36603c9e53674161950a0a
emmabell42 <emmabell42@gmail.com>
2015-06-10 15:29:44 +0100

    Update illuminaQualFilter.R

--- illuminaQualFilter.R
+++ illuminaQualFilter.R
@@ -1,6 +1,8 @@
-ofile <- as.character(read.table("file_to_filter.txt")[[1]])
+ofile <- as.character(read.table("tofilter")[[1]])
 library(ShortRead)
-reads <- readFastq(ofile)
+for(i in 1:length(ofile)){
+cat("Reading",ofile[i],as.character(Sys.time()),sep=" "
+reads <- readFastq(ofile[i])
 ideal.readlength <- as.numeric(names(tail(sort(table(width(reads[1:100]))),n=1)))
 quals1 <- as(FastqQuality(quality(quality(reads[1:min(c(length(reads),20000000))]))),"matrix")
 if(length(reads)>20000000){
@@ -20,7 +22,8 @@ trim <- trim[good.reads]
 trim[is.infinite(trim)] <- ideal.readlength
 reads <- ShortReadQ(sread=subseq(sread(reads),start=rep(1,length(trim)),end=trim),quality=new(Class=class(quality(reads)),quality=subseq(quality(quality(reads)),start=rep(1,length(trim)),end=trim)),id=id(reads))
 #no_n <- which(alphabetFrequency(sread(reads[1:100]))[,'N']==0)
-newfile <- strsplit(ofile,split=".fastq")[[1]][1]
+newfile <- strsplit(ofile[i],split=".fastq")[[1]][1]
 newfile <- paste(newfile,"_filtered.fq",sep="")
 #writeFastq(reads[no_n],newfile)
 writeFastq(reads,newfile,compress=F)
+}
