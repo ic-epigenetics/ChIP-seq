4d6f39aeefd7a390ff88b738e520a842be820441
emmabell42 <emmabell42@gmail.com>
2015-06-18 10:57:30 +0100

    Update illuminaQualFilter.R

--- illuminaQualFilter.R
+++ illuminaQualFilter.R
@@ -1,22 +1,48 @@
 ofile <- as.character(read.table("tofilter")[[1]])
 library(ShortRead)
-for(i in 1:length(ofile)){
-cat("Reading",ofile[i],as.character(Sys.time()),sep=" "
+for(i in 26:length(ofile)){
+cat("Reading",ofile[i],as.character(Sys.time()),sep=" ",fill=T)
 reads <- readFastq(ofile[i])
 ideal.readlength <- as.numeric(names(tail(sort(table(width(reads[1:100]))),n=1)))
 quals1 <- as(FastqQuality(quality(quality(reads[1:min(c(length(reads),20000000))]))),"matrix")
 if(length(reads)>20000000){
-        quals2 <- as(FastqQuality(quality(quality(reads[c(20000001:length(reads))]))),"matrix")
+        quals2 <- as(FastqQuality(quality(quality(reads[20000001:min(c(length(reads),40000000))]))),"matrix")
+}
+if(length(reads)>40000000){
+        quals3 <- as(FastqQuality(quality(quality(reads[40000001:min(c(length(reads),60000000))]))),"matrix")    
+}
+if(length(reads)>60000000){
+        quals4 <- as(FastqQuality(quality(quality(reads[60000001:min(c(length(reads),80000000))]))),"matrix")    
+}
+if(length(reads)>80000000){
+        quals5 <- as(FastqQuality(quality(quality(reads[80000001:min(c(length(reads),100000000))]))),"matrix")
+}
+if(length(reads)>100000000){
+        quals6 <- as(FastqQuality(quality(quality(reads[100000001:min(c(length(reads),120000000))]))),"matrix")    
 }
 trim <- apply(quals1,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
 if(length(reads)>20000000){
         trim2 <- apply(quals2,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
-        trim <- c(trim,trim2)
+		trim <- c(trim,trim2)
+}
+if(length(reads)>40000000){
+        trim3 <- apply(quals3,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
+        trim <- c(trim,trim3)
+}
+if(length(reads)>60000000){
+        trim4 <- apply(quals4,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
+        trim <- c(trim,trim4)
+}
+if(length(reads)>80000000){
+        trim5 <- apply(quals5,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
+        trim <- c(trim,trim5)
+}
+if(length(reads)>100000000){
+        trim6 <- apply(quals6,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
+        trim <- c(trim,trim6)
 }
 bad.q <- which(!is.infinite(trim) & trim<(0.8*ideal.readlength))
-#5607704 of 20944820 reads
 good.reads <- setdiff(c(1:length(reads)),bad.q)
-#15337116 good reads
 reads <- reads[good.reads]
 trim <- trim[good.reads]
 trim[is.infinite(trim)] <- ideal.readlength
