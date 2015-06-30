ef65d3ed9f07b068907e4fa406cc57ba2d347236
emmabell42 <emmabell42@gmail.com>
2015-06-30 10:46:03 +0100

    Update RemoveSRDuplicates.R

--- RemoveSRDuplicates.R
+++ RemoveSRDuplicates.R
@@ -1,7 +1,7 @@
 require(ShortRead)
 
 removeSRduplicates <- function(inputFile){
-  RA <- readAligned(inputFile,type="BAM",filter=nFilter())
+  RA <- readAligned(dirPath=getwd(),pattern=inputFile,type="BAM",filter=nFilter())
   RA <- RA[!is.na(position(RA))]
   RA[!duplicated(position(RA))]
 }
